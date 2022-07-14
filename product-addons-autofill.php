7a03a03fcf1b5cc948c0e2758a75e247e5d33678
Olivier van Helden <olivier@van-helden.net>
2022-07-14 09:38:40 -0400

    Initial commit

---
+++ product-addons-autofill.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Auto-fill product add-ons
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Auto-fill WooCommerce product add-ons fields with values passed in the URL
+ * Author:          Magiiic
+ * Author URI:      https://magiiic.com
+ * Text Domain:     product-addons-autofill
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Product_Addons_Autofill
+ */
+
+// Your code starts here.
