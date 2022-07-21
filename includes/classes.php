22bb5d43d6c5d415ccce3176bdb8b1342cc6d1af
Olivier van Helden <olivier@van-helden.net>
2022-07-20 22:23:39 -0400

    * added flexible amount field

--- includes/classes.php
+++ includes/classes.php
@@ -25,17 +25,18 @@ class PROPRO {
     add_filter( 'woocommerce_add_to_cart_validation', __CLASS__ . '::validate_custom_field', 10, 3 );
     add_filter( 'woocommerce_add_cart_item_data', __CLASS__ . '::add_custom_field_item_data', 10, 4 );
     add_filter( 'woocommerce_cart_item_name', __CLASS__ . '::cart_item_name', 1, 3 );
+		// add_filter( 'wc_add_to_cart_message', __CLASS__ . '::add_to_cart_message', 10, 2 );
 
-		// add_action( 'woocommerce_before_calculate_totals', __CLASS__ . '::before_calculate_totals', 10, 1 );
-    add_action( 'woocommerce_checkout_create_order_line_item', __CLASS__ . '::add_custom_data_to_order', 10, 4 );
+		add_filter( 'woocommerce_get_price_html', __CLASS__ . '::get_price_html', 10, 2 );
+		add_action( 'woocommerce_before_calculate_totals', __CLASS__ . '::before_calculate_totals', 10, 1 );
 
-    // add_filter( 'wc_add_to_cart_message', __CLASS__ . '::add_to_cart_message', 10, 2 );
-    // add_filter( 'woocommerce_get_price_html', __CLASS__ . '::get_price_html', 10, 2 );
-    //
-    add_action( 'plugins_loaded', __CLASS__ . '::load_plugin_textdomain' );
-    //
+		add_action( 'woocommerce_checkout_create_order_line_item', __CLASS__ . '::add_custom_data_to_order', 10, 4 );
+
+		// Set pay button text
     add_filter( 'woocommerce_product_add_to_cart_text', __CLASS__ . '::add_to_card_button', 10, 2);
     add_filter( 'woocommerce_product_single_add_to_cart_text', __CLASS__ . '::single_add_to_card_button', 10, 2);
+
+		add_action( 'plugins_loaded', __CLASS__ . '::load_plugin_textdomain' );
   }
 
   static function load_plugin_textdomain() {
@@ -85,34 +86,59 @@ class PROPRO {
     global $post;
 		if(!propro_is_project_product( wc_get_product( $post->ID ) )) return;
 
-    $value = (isset($_REQUEST['project'])) ? esc_attr($_REQUEST['project']) : NULL;
+    $project = (isset($_REQUEST['project'])) ? esc_attr($_REQUEST['project']) : NULL;
+		$amount = (isset($_REQUEST['amount'])) ? esc_attr($_REQUEST['amount']) : ((isset($_REQUEST['nyp'])) ? esc_attr($_REQUEST['nyp']) : NULL);
 
-    // $value = isset( $_POST['propro_project_name'] ) ? sanitize_text_field( $_POST['propro_project_name'] ) : '';
+		printf(
+		  '<div class="propro-field propro-field-amount">
+		    <p class="form-row form-row-wide">
+		      <label for="propro_amount" class="required">%s%s</label>
+		      <input type="number" class="input-text" name="propro_amount" value="%s" placeholder="%s" required>
+		    </p>
+		  </div>',
+		  __('Amount', 'lodgify-link'),
+		  ' <abbr class="required" title="required">*</abbr>',
+		  $amount,
+		  __("Amount to pay", 'lodgify-link'),
+		  '',
+		);
+
+    // $project = isset( $_POST['propro_project_name'] ) ? sanitize_text_field( $_POST['propro_project_name'] ) : '';
     printf(
       '<div class="propro-field propro-field-project-name">
-        <label for="propro_project_name" class="required">%s%s</label>
-        <p class="form-row form-row-wide">
+				<p class="form-row form-row-wide">
+        	<label for="propro_project_name" class="required">%s%s</label>
           <input type="%s" class="input-text" name="propro_project_name" value="%s" placeholder="%s" required>
         </p>
         %s
       </div>',
       __('Project', 'project-products'),
-      (empty($value)) ? ' <abbr class="required" title="required">*</abbr>' : ': <span class=project_name>' . $value . '</span>',
-      (empty($value)) ? 'text' : 'hidden',
-      $value,
+      (empty($project)) ? ' <abbr class="required" title="required">*</abbr>' : ': <span class=project_name>' . $project . '</span>',
+      (empty($project)) ? 'text' : 'hidden',
+      $project,
       __("Enter a project name", 'project-products'),
-      (empty($value)) ? __('Specify the project related to this purchase.', 'project-products') : '',
+      (empty($project)) ? __('Specify the project related to this purchase.', 'project-products') : '',
     );
   }
 
   static function validate_custom_field( $passed, $product_id, $quantity ) {
     if($passed && propro_is_project_product( $product_id )) {
-      if(!empty($_POST['propro_project_name']))
-      $project = sanitize_text_field($_POST['propro_project_name']);
-      else if(!empty($_REQUEST['project']))
-      $project = sanitize_text_field($_REQUEST['project']);
-      else
-      $project = NULL;
+      if(!empty($_POST['propro_project_name'])) $project = sanitize_text_field($_POST['propro_project_name']);
+      else if(!empty($_REQUEST['project'])) $project = sanitize_text_field($_REQUEST['project']);
+      else $project = NULL;
+
+			if(!empty($_POST['propro_amount'])) $amount = sanitize_text_field($_POST['propro_amount']);
+      else if(!empty($_REQUEST['amount'])) $amount = sanitize_text_field($_REQUEST['amount']);
+      else $amount = NULL;
+
+			if(!is_numeric($amount) || $amount <= 0) {
+				$product_title = wc_get_product( $product_id )->get_title();
+				wc_add_notice( sprintf(
+          __('"%s" could not be added to the cart. Please provide a valid amount to pay.', 'lodgify-link'),
+          sprintf('<a href="%s">%s</a>', get_permalink($product_id), $product_title),
+        ), 'error' );
+        return false;
+			}
 
       if( empty( $project ) ) {
         $product_title = wc_get_product( $product_id )->get_title();
@@ -136,39 +162,16 @@ class PROPRO {
   * @param Boolean $quantity Quantity
   */
   static function add_custom_field_item_data( $cart_item_data, $product_id, $variation_id, $quantity ) {
-    if(!empty($_POST['propro_project_name']))
-    $cart_item_data['propro_project_name'] = sanitize_text_field($_POST['propro_project_name']);
-    else if(!empty($_REQUEST['project']))
-    $cart_item_data['propro_project_name'] = sanitize_text_field($_REQUEST['project']);
+		if(!propro_is_project_product( wc_get_product( $product_id ) )) return $cart_item_data;
+
+    if(!empty($_POST['propro_project_name'])) $cart_item_data['propro_project_name'] = sanitize_text_field($_POST['propro_project_name']);
+    else if(!empty($_REQUEST['project'])) $cart_item_data['propro_project_name'] = sanitize_text_field($_REQUEST['project']);
+
+		if(!empty($_POST['propro_amount'])) $cart_item_data['propro_amount'] = sanitize_text_field($_POST['propro_amount']);
+    else if(!empty($_REQUEST['amount'])) $cart_item_data['propro_amount'] = sanitize_text_field($_REQUEST['amount']);
+		else if(!empty($_REQUEST['nyp'])) $cart_item_data['propro_amount'] = sanitize_text_field($_REQUEST['nyp']);
 
     return $cart_item_data;
-  }
-
-  /**
-  * Update the price in the cart
-  * @since 1.0.0
-  */
-  function before_calculate_totals( $cart ) {
-    if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
-      return;
-    }
-    // Iterate through each cart item
-    foreach( $cart->get_cart() as $cart_key => $cart_item ) {
-      $cached = wp_cache_get('propro_cart_item_processed_' . $cart_key, 'project-products');
-      if(!$cached) {
-        if( is_numeric( $cart_item['project_price'] &! $cart_item['project_price_added']) ) {
-          $price = (float)$cart_item['data']->get_price( 'edit' );
-          $total = $price + $cart_item['project_price'];
-          error_log('adding ' . $cart_item['project_price']
-          . "\n" . 'initial price ' . $price
-          . "\n" . 'adjusted price ' . $total);
-          // $cart_item['data']->adjust_price( $cart_item['project_price'] );
-          $cart_item['data']->set_price( ( $total ) );
-          $cart_item['project_price_added'] = true;
-        }
-        wp_cache_set('propro_cart_item_processed_' . $cart_key, true, 'project-products');
-      }
-    }
   }
 
   /**
@@ -176,7 +179,7 @@ class PROPRO {
   * @since 1.0.0
   */
   static function cart_item_name( $name, $cart_item, $cart_item_key ) {
-    error_log(__FUNCTION__ . ' ' . print_r($cart_item, true));
+    // error_log(__FUNCTION__ . ' ' . print_r($cart_item, true));
 
     if( isset( $cart_item['propro_project_name'] ) ) {
       $name = sprintf(
@@ -199,7 +202,7 @@ class PROPRO {
     }
   }
 
-  function get_price_html( $price_html, $product ) {
+  static function get_price_html( $price_html, $product ) {
     if($product->get_meta( '_linkproject' ) == 'yes') {
       $price = max($product->get_price(), get_option('propro_project_minimum_price', 0));
       if( $price == 0 ) {
@@ -215,6 +218,33 @@ class PROPRO {
       }
     }
     return $price_html;
+  }
+
+	/**
+  * Update the price in the cart
+  * @since 1.0.0
+  */
+  static function before_calculate_totals( $cart ) {
+    if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
+      return;
+    }
+    // Iterate through each cart item
+    foreach( $cart->get_cart() as $cart_key => $cart_item ) {
+      $cached = wp_cache_get('propro_cart_item_processed_' . $cart_key, 'project-products');
+      if(!$cached) {
+        if( is_numeric( $cart_item['propro_amount'] &! $cart_item['propro_amount_added']) ) {
+          $price = (float)$cart_item['data']->get_price( 'edit' );
+          $total = $price + $cart_item['propro_amount'];
+          error_log('adding ' . $cart_item['propro_amount']
+          . "\n" . 'initial price ' . $price
+          . "\n" . 'adjusted price ' . $total);
+          // $cart_item['data']->adjust_price( $cart_item['propro_amount'] );
+          $cart_item['data']->set_price( ( $total ) );
+          $cart_item['propro_amount_added'] = true;
+        }
+        wp_cache_set('propro_cart_item_processed_' . $cart_key, true, 'project-products');
+      }
+    }
   }
 }
 
