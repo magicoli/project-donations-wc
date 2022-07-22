94bf1a11e5b392002239527e10b0fa5fdf1c5ede
Olivier van Helden <olivier@van-helden.net>
2022-07-22 16:42:06 -0400

    * fix update library and assets

--- lib/package-updater.php
+++ lib/package-updater.php
@@ -8,6 +8,6 @@ if ( ! defined( 'WPINC' ) ) {
 require_once plugin_dir_path( dirname(__FILE__) ) . 'lib/wp-package-updater/class-wp-package-updater.php';
 $prdwc_updater = new WP_Package_Updater(
 	'https://magiiic.com',
-	wp_normalize_path( plugin_dir_path( dirname(__FILE__) ) . "/project-donations.php" ),
+	wp_normalize_path( plugin_dir_path( dirname(__FILE__) ) . "/project-donations-wc.php" ),
 	wp_normalize_path( plugin_dir_path( dirname(__FILE__) ) )
 );
