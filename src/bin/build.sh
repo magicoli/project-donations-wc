902e22c5bb305c563dc149394412b974cee35231
Olivier van Helden <olivier@van-helden.net>
2023-06-09 09:40:50 -0400

    src/bin/build.sh

---
+++ src/bin/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ignore="vendor/*,node_modules/*,sources/,*.asset.php,*/*/*.asset.php,*.map,includes/fullcalendar/fullcalendar.*"
+
+PGM=$(basename $0)
+trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
+trap 'ret=$?; [ $ret -ne 0 ] && echo "$PGM: $previous_command failed (error $ret)" && exit $ret || echo "$PGM: success"' EXIT
+
+minphp=$(grep "Requires PHP:" readme.txt | sed "s/.*PHP: *//")
+echo "# check compatibility with minimum PHP version required $minphp"
+phpcs -p . --standard=PHPCompatibility --ignore=$ignore,*js,*css --runtime-set testVersion ${minphp}- \
+&& echo "# normalize code" \
+&& { phpcbf --standard=WordPress --ignore=$ignore ./ || phpcbf --standard=WordPress --ignore=$ignore ./ ; } \
+&& echo "# $minphp composer update" \
+&& php$minphp /usr/local/bin/composer update --no-dev \
+&& echo "# npm build" \
+&& npm run build
